af8042b9e79b060ecdb32f48a00e9862712eaf5b
Emekarr <aceanyanwu02@gmail.com>
2024-02-27 13:59:51 +0100

    fix : 1. fixed compling issue from responder/types and middleware/express/ip 2. updated package.json to clear former build folder before recompiling 3. updated gitignore to ignore build folder

--- src/application/responder/types.ts
+++ src/application/responder/types.ts
@@ -1,4 +1,4 @@
-export interface ResponderInterface {
+export default interface ResponderInterface {
   respond(
     ctx: any,
     message: string,
--- src/infrastructure/middleware/express/ip.ts
+++ src/infrastructure/middleware/express/ip.ts
@@ -3,9 +3,7 @@ import { ErrorMiddleware } from "../../../application/middleware/error";
 import Responder from "../../responder";
 
 export default (err: any, req: Request, res: Response, next: NextFunction) => {
-  ErrorMiddleware({
-    err,
-    respond: Responder.respond,
-  });
+  // this middleware should get the ip of the client and parse it through an ip database like
+  // maxmind to figure out if the user is using a masked ip or a vpn or is in an odd location
   next();
 };
